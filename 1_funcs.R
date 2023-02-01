c4335a5ed8cffd8e81871c0b66034950afbc51e9
Mike Rosen <mrosena@gmail.com>
2023-01-31 19:03:12 -0500

    attempt to merge

--- 1_funcs.R
+++ 1_funcs.R
@@ -15,7 +15,7 @@ loadE4CsvToDB <- function(fPath, con) {
   # Pass a file path and upload the data to the connection
   # If the file path is set up as it comes from empatica, this will work
   # E4 device numbers is the folder name (after *timestamp_*) and file name is measure
-  print(fPath)
+  
   valueList <- list( # used to set variable names usable in db
     'EDA' = 'micro_s', 'TEMP' = 'degrees_c', 'BVP' = 'bvp','HR' = 'avg_hr',
     'ACC' = c('x','y','z')
@@ -53,18 +53,9 @@ loadE4CsvToDB <- function(fPath, con) {
     print('Not doing IBI yet')
     data <- data.frame()
   }
-  
-  # deal with rare case of malformed or NA data
-  # if (measure == 'ACC') {
-  #   data[,valueList[[measure]]] <- as.integer(data[,valueList[[measure]]])
-  # } else {
-  #   data[,valueList[[measure]]] <- as.numeric(data[,valueList[[measure]]])
-  # }
-  data <- data[complete.cases(data),]
 
   # Push to DB
   t <- paste0(tolower(device),'_',tolower(measure))
-  print(paste(t,": ",nrow(data)," rows of data"))
   if (!DBI::dbExistsTable(con,t)) { # check if table exists in database; create if it doesn't
     DBI::dbCreateTable(con, t, data)
   } 
